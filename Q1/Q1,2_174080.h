62de6cca13a98325be612fe4638d448833853855
174080maryamMukhtar <43024103+174080maryamMukhtar@users.noreply.github.com>
2018-10-25 11:02:49 +0500

    Update Q1,2_174080.h

--- Q1/Q1,2_174080.h
+++ Q1/Q1,2_174080.h
@@ -2,21 +2,17 @@ class enrollment{
 	private:
 		int *rollnumbers;   //pointer to an int array //that stores the roll numbers
 		int length;       //current number of students enrolled
-		int maxSize;     //total capacity of the class
+		int maxSize;       //total capacity of the class
 	public:
-		void setLength(int a); 
-		void print() const;   //function to print roll numbers of students
-
-		void generateAttendanceSheet();     //Arrange the rollnumbers in Ascending order
-
-		void insertinOrder(int rollnumber);   //function to insert the roll number in order in the array
-
-		enrollment(int size=10);     //create array of the size specified by the paramerter "size"
-                                         //use this pointer in the constructor to assign values
-		void input();
-		void setLength(); 
-		bool  Exist(int rollnumber);    //function to check the existence of a roll number
-		void testcopyconstructor(enrollment obj);   
-		~enrollment();
-		enrollment(const enrollment& obj);
+	enrollment(int size=10);            //create array of the size specified by the paramerter "size"
+                                            //use this pointer in the constructor to assign values
+	enrollment(const enrollment& obj);     //copy constructor
+	void setLength(int a); 
+	void input();                       //set length function
+	void print() const;                 //function to print roll numbers of students
+        void generateAttendanceSheet();     //Arrange the rollnumbers in Ascending order
+	void insertinOrder(int rollnumber);   //function to insert the roll number in order in the array
+        bool  Exist(int rollnumber);           //function to check the existence of a roll number
+	void testcopyconstructor(enrollment obj);    //copying one obj elements into another and adding 1 in roll numbers
+	~enrollment();     //destructor			
 };
