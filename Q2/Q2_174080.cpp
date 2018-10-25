4fef6a15c590fc75f107d14290d46de948119af8
174080maryamMukhtar <43024103+174080maryamMukhtar@users.noreply.github.com>
2018-10-25 10:53:10 +0500

    Update Q2_174080.cpp

--- Q2/Q2_174080.cpp
+++ Q2/Q2_174080.cpp
@@ -4,27 +4,27 @@ using namespace std;
 int main()
 {
 	enrollment computer_programming;
-	int a;
+	int a;                                     //length
 	cout<<"Enter the number of students enrolled: ";
 	cin>>a;
 	computer_programming.setLength(a);
-	cout<<"Enter Roll Numbers"<<endl;
-	computer_programming.input();
+	cout<<"Enter Roll Numbers"<<endl;  
+	computer_programming.input(); 
 
-	enrollment cp_lab=computer_programming;
+	enrollment cp_lab=computer_programming;     //calling copy constructor
 	int rollnum;
 	cout<<"Enter the Roll Number you want to add : ";
 	cin>>rollnum;
 	cout<<endl;
-	computer_programming.insertinOrder(rollnum);
+	computer_programming.insertinOrder(rollnum);       //inserting roll number in the array of roll numbers
 	cout<<"-----Computer Programming Object-----"<<endl;
 	computer_programming.print();
 	cout<<endl;
 	cout<<"-----CP Lab Object-----"<<endl;
 	cp_lab.print();
 	cout<<endl;
-	enrollment temp=computer_programming;
-	computer_programming.testcopyconstructor(temp);
+	enrollment temp=computer_programming;          //copy constructor
+	computer_programming.testcopyconstructor(temp);   //calling test copy constructor function for adding 1 in computer programming roll numbers array
 	cout<<"----Printing Roll Numkers After Adding 1-----"<<endl;
 	computer_programming.print();
 	system("pause");
