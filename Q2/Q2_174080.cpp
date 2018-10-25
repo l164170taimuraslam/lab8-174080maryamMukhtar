71e15cf7d46b7f0c7b739bc71d5e6eb47863e551
174080maryamMukhtar <43024103+174080maryamMukhtar@users.noreply.github.com>
2018-10-25 17:07:50 +0500

    Update Q2_174080.cpp

--- Q2/Q2_174080.cpp
+++ Q2/Q2_174080.cpp
@@ -1,32 +1,84 @@
 #include<iostream>
 #include"q1.h"
 using namespace std;
+void Input(int &a);                                             //Input Function
+void check(int &a, enrollment obj);                             //Function to check if length is greater than max size
+void Task1(int &a, enrollment &computer_programming);           //Function performing Task 1
+void Task3(int &rollnum, enrollment &computer_programming);     //Inserting roll number in the array of roll numbers
+void Task4(enrollment obj);                                    //Printing Roll Numbers
 int main()
 {
+	//TASK 1
 	enrollment computer_programming;
-	int a;                                     //length
-	cout<<"Enter the number of students enrolled: ";
-	cin>>a;
-	computer_programming.setLength(a);
-	cout<<"Enter Roll Numbers"<<endl;  
-	computer_programming.input(); 
+	int a;   //length
+	Task1(a, computer_programming);
+	cout << "-----Computer Programming Object-----" << endl;
+	computer_programming.print();
 
-	enrollment cp_lab=computer_programming;     //calling copy constructor
+	//TASK 2 
+	enrollment cp_lab = computer_programming;               //calling copy constructor
+
+	//TASK 3
 	int rollnum;
-	cout<<"Enter the Roll Number you want to add : ";
-	cin>>rollnum;
-	cout<<endl;
-	computer_programming.insertinOrder(rollnum);       //inserting roll number in the array of roll numbers
-	cout<<"-----Computer Programming Object-----"<<endl;
-	computer_programming.print();
-	cout<<endl;
-	cout<<"-----CP Lab Object-----"<<endl;
-	cp_lab.print();
-	cout<<endl;
-	enrollment temp=computer_programming;          //copy constructor
+	Task3(rollnum, computer_programming);
+
+	//TASK 4
+	cout << "-----Computer Programming Object-----" << endl;
+	Task4(computer_programming);
+	cout << "\n-----CP Lab Object-----" << endl;
+	Task4(cp_lab);
+
+	//TASK 5
+	enrollment temp = computer_programming;            //copy constructor
 	computer_programming.testcopyconstructor(temp);   //calling test copy constructor function for adding 1 in computer programming roll numbers array
-	cout<<"----Printing Roll Numkers After Adding 1-----"<<endl;
-	computer_programming.print();
+	cout << "\n----Printing Roll Numbers After Test Copy Constructor----" << endl;
+	Task4(computer_programming);
+	cout << endl;
 	system("pause");
 	return 0;
+}
+void Input(int &a)
+{
+	cin >> a;
+}
+void check(int &a, enrollment obj)
+{
+	if (a > obj.getmaxsize() || a < 0)
+	{
+		while (a > obj.getmaxsize() || a < 0)
+		{
+			cout << "This size is not possible" << endl;
+			cout << "Enter Length Again: ";
+			cin >> a;
+		}
+	}
+}
+void Task1(int &a, enrollment &computer_programming)
+{
+	cout << "Enter the number of students enrolled: ";
+	Input(a);
+	check(a, computer_programming);
+	computer_programming.setLength(a);
+	cout << "Enter Roll Numbers" << endl;
+	computer_programming.input();
+}
+void Task3(int &rollnum, enrollment &computer_programming)
+{
+	cout << "\nEnter the Roll Number you want to add : ";
+	Input(rollnum);
+	if (rollnum < 0)
+	{
+		cout << "Invalid Roll Number !" << endl;
+		while (rollnum < 0)
+		{
+			Input(rollnum);
+		}
+	}
+	cout << endl;
+	computer_programming.insertinOrder(rollnum);
+}
+void Task4(enrollment obj)
+{
+	obj.print();
+	cout << endl;
 }
